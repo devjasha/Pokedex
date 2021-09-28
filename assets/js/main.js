90ac9c653ff6751fa3cc7b00d4ae814c60327ebf
jasha.chec <jasha@chec.de>
2021-09-28 08:21:36 +0200

    last update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,16 @@
+
 function setupTabs() {
+  const detailInfoContainer = document.querySelector(".info");
+  const tabToActivate = detailInfoContainer.querySelector(`.content[data-tab="1"]`);
+
+  document.querySelectorAll(".link")[0].classList.add("is-active");
+  tabToActivate.classList.add("is-active");
+
   document.querySelectorAll(".link").forEach(button => {
     button.addEventListener("click", () => {
       const link = button.parentElement;
-      const detailInfoContainer = document.querySelector(".info");
       const containerNumber = button.dataset.forTab;
-      const tabToActivate = detailInfoContainer.querySelector(`.content[data-tab="${containerNumber}"]`)
+      const tabToActivate = detailInfoContainer.querySelector(`.content[data-tab="${containerNumber}"]`);
 
       link.querySelectorAll(".link").forEach(button => {
         button.classList.remove("is-active");
@@ -18,8 +24,8 @@ function setupTabs() {
       tabToActivate.classList.add("is-active");
     });
   });
-}
+};
 
 document.addEventListener("DOMContentLoaded", () => {
   setupTabs();
-})
+});
